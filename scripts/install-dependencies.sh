746b1937ad2b6c0a7ffa830ce766b9875a29c872
Carlos Florez <carlos@florez.co.uk>
2023-04-24 16:19:05 -0700

    build(nvim): Remove neovim from ppa dependencies, add as src dependency
    Add url loop check for src dependencies
    Move check_dependencies method to last in execution order

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -8,14 +8,6 @@ color_normal=$(tput sgr0)
 add_ppa() {
 
     printf "%s\n" ""
-    printf "%s\n" " -> Beginning Add neovim ppa: "
-    printf "%s\n" ""
-    sleep 1
-    
-    add-apt-repository ppa:neovim-ppa/stable
-    apt-get update -y
-
-    printf "%s\n" ""
     printf "%s\n" " -> Beginning Add nodejs ppa: "
     printf "%s\n" ""
     sleep 1
@@ -223,15 +215,46 @@ check_dependencies() {
 
 get_src_dependencies() {
 	
-	printf "%s\n" ""
-	printf "%s\n" " -> Beginning src dependencies download: "
-	printf "%s\n" ""
+    printf "%s\n" ""
+    printf "%s\n" " -> Beginning src dependencies URL check: "
+    printf "%s\n" ""
+
+    neovim_branch_version="stable"
+
+    jdtls_url="https://download.eclipse.org/jdtls/milestones/1.9.0/jdt-language-server-1.9.0-202203031534.tar.gz"
+    maven_url="https://dlcdn.apache.org/maven/maven-3/3.9.1/binaries/apache-maven-3.9.1-bin.tar.gz"
+    lombok_url="https://projectlombok.org/downloads/lombok.jar"
+    neovim_url="https://github.com/neovim/neovim.git"
+    urls=($jdtls_url $maven_url $lombok_url)
+    for url in "${urls[@]}"
+    do
+	response="$(curl --head --silent --write-out %{http_code} --output /dev/null $url)"
+	if [ $response -eq 200 ]
+	then
+	    printf "%s\n" "${color_green}URL VALID${color_normal}: ${url}"
+	else
+            printf "%s\n" "${color_red}ERROR${color_normal}: ${url} is not valid. http code: ${response}"
+            printf "%s\n" "${color_red}ABORTING${color_normal}"
+	    exit 1
+	fi
+	sleep 1
+    done
+
+    printf "%s\n" ""
+    printf "%s\n" " -> Beginning src dependencies download: "
+    printf "%s\n" ""
+
     # jdtls
-	curl -L -o /tmp/jdtls.tar.gz https://download.eclipse.org/jdtls/milestones/1.9.0/jdt-language-server-1.9.0-202203031534.tar.gz
+	curl -L -o /tmp/jdtls.tar.gz $jdtls_url 
     # maven
-	curl -L -o /tmp/maven.tar.gz https://dlcdn.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.tar.gz
-    # Lombok
-    curl -L -o /tmp/lombok.jar https://projectlombok.org/downloads/lombok.jar
+	curl -L -o /tmp/maven.tar.gz $maven_url
+   	# Lombok
+	curl -L -o /tmp/lombok.jar  $lombok_url
+     	#NeoVim
+	git clone --depth=1 $neovim_url --branch $neovim_branch_version --single-branch /tmp/neovim
+	cd /tmp/neovim
+	make CMAKE_BUILD_TYPE=Release
+       	make install
 }
 
 get_npm_dependencies() {
@@ -255,9 +278,9 @@ update
 get_apt_dependencies
 add_ppa
 get_ppa_dependencies
-check_dependencies
 get_src_dependencies
 get_npm_dependencies
+check_dependencies
 printf "%s\n" ""
 printf "%s\n" "${color_green}SUCCESS${color_normal}: Installation complete!"
 printf "%s\n" ""
