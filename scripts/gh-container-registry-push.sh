f06a703f941194e9673974c2d139d71b8a359b45
Carlos Florez <carlos@florez.co.uk>
2023-04-27 18:05:48 -0700

    build(push): Add command to tag from image_version to latest

--- scripts/gh-container-registry-push.sh
+++ scripts/gh-container-registry-push.sh
@@ -38,6 +38,8 @@ printf "%s\n" ""
 sleep 1
 docker push ghcr.io/$(GIT_USER_USERNAME)/dev-env-ubuntu-base-img:v${IMAGE_VERSION}
 
+docker tag ghcr.io/$(GIT_USER_USERNAME)/dev-env-ubuntu-base-img:v${IMAGE_VERSION} ghcr.io/$(GIT_USER_USERNAME)/dev-env-ubuntu-base-img:latest
+
 printf "%s\n" ""
 printf "%s\n" " -> Pushing new version named as latest"
 printf "%s\n" ""
