2b75004993485e4442cf27cd740886791a66f594
CARLOS FLOREZ <carlos@florez.co.uk>
2025-11-21 15:07:58 -0400

    build(docker): Add missing Docker installation dependency

--- scripts/install-host-dependencies.sh
+++ scripts/install-host-dependencies.sh
@@ -14,7 +14,7 @@ install_docker() {
 	https://download.docker.com/linux/ubuntu \
   	$(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 	apt-get update -y
-	apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+	apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-buildx-plugin -y
 	docker run hello-world
 	adduser "${USER}" docker
 
